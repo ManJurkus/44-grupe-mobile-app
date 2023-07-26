4dc618f13fe081e02a2ac708957d201bd47395af
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-26 10:20:42 +0300

    feature bubble

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import style from './App.module.css';
 import { Home } from './pages/Home';
-import { Feature } from "./pages/Feature"
+import { Feature } from "./pages/Feature";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const dataFeature = [
     {
       id:1,
       title:'Feature-1',
-      text: 'Feature-1 Lorem ipsum dolor sit, amet consectetur adipisicing.'
+      text: 'Feature-1 Lorem ipsum dolor sit, amet consectetur adipisicing.',
+      buttonTitle: 'Continue',
     },
     {
       id:2,
       title:'Feature-2',
-      text: 'Feature-2 Lorem ipsum dolor sit, amet consectetur adipisicing.'
+      text: 'Feature-2 Lorem ipsum dolor sit, amet consectetur adipisicing.',
+      buttonTitle: 'Continue',
     },
     {
       id:3,
       title:'Feature-3',
-      text: 'Feature-3 Lorem ipsum dolor sit, amet consectetur adipisicing.'
+      text: 'Feature-3 Lorem ipsum dolor sit, amet consectetur adipisicing.',
+      buttonTitle: 'Register',
     }
   ];
   
-  let needBubbleCount = 0;
-  for (const index in dataFeature){
-    needBubbleCount++;
-  }
-  console.log(needBubbleCount);
+ 
 
 
   return (
-    
     <div className={style.app}>
-      <Home />
-      <Feature title='Feature-1' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />
-      <Feature title='Feature-2' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />
-      <Feature title='Feature-3' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />
+      <BrowserRouter>
+        <Routes>
+
+          <Route index path="/" element={ <Home />} />
+          <Route  path="/feature1" element={ <Feature dataFeature={dataFeature} buttonTitle="Continue" title='Feature-1' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />} />
+          <Route  path="/feature2" element={ <Feature dataFeature={dataFeature} buttonTitle="Continue" title='Feature-2' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />} />
+          <Route  path="/feature3" element={ <Feature dataFeature={dataFeature} buttonTitle="Register" title='Feature-3' text='Lorem ipsum dolor sit, amet consectetur adipisicing.' />} />
+        </Routes>
+      </BrowserRouter>
+
  
     </div>
   );
