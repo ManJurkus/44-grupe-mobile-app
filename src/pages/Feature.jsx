c99e8885602289bef14101eedd6caee0ae20a064
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-26 10:20:17 +0300

    Feature bubble

--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -3,14 +3,24 @@ import style from './Feature.module.css';
 import { BiCircle } from 'react-icons/bi';
 
 
-export function Feature({title, text}) {
+export function Feature({title, text, dataFeature, buttonTitle}) {
+
+    function bubbleAll() {
+      const bubbleDiv = dataFeature.map((item, index) => <BiCircle key={index} className={isCurrentPage ? style.greenBubble : ''} />);
+      console.log(bubbleDiv);
+      return <div className={style.bubble} >{bubbleDiv}</div>;
+    }
+  
+
+  
+
     return (
         <div className={style.featurePage}>
-          <img className={style.img} src='./img/logo.jpg' alt='Imagine' />
+          <img className={style.img} src='./img/log.jpg' alt='Imagine' />
           <h2>{title}</h2>
           <p>{text}</p>
-          <BiCircle />
-          <Button title="Continue" />
+          {bubbleAll()}
+          <Button title={buttonTitle} />
         </div>
     );
   }
