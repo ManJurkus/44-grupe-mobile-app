de22f0979e2ad3e83d42e604a5a660275049d9de
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-27 15:37:17 +0300

    some of validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,20 @@
 import { Button } from '../components/Button';
 import style from './Register.module.css';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import button from '../components/Button.module.css';
 
 export function Register() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [check, setCheck] = useState(false);
+    const [errors, setErrors] = useState([]);
+    const [users, setUsers] = useState(() => JSON.parse(localStorage.getItem('users')) || []);
+
+    useEffect(() => {
+        localStorage.setItem('users', JSON.stringify(users));
+    }, [users]);
+
 
     function updateName(event) {
         setName(event.target.value);
@@ -15,9 +24,55 @@ export function Register() {
         setEmail(event.target.value);
     }
 
+    function updatePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function updateCheck() {
+        setCheck(!check);
+    }
+
+    function registerUser(event) {
+        const minUsernameLength = 3;
+        const maxUsernameLength = 20;
+        const minPasswordLength = 6;
+        const maxPasswordLength = 100;
+        event.preventDefault();
+        const newErrors = [];
+
+        if (name.length < minUsernameLength || name.length > maxUsernameLength) {
+            newErrors.push('klaida: name...');
+        }
+
+        if (password.length < minPasswordLength || password.length > maxPasswordLength) {
+            newErrors.push('klaida: password...');
+        }
+
+        if (!email.includes('@') ) {
+            newErrors.push('klaida: email...');
+        }
+
+        if ( check === false ) {
+            newErrors.push('klaida: taisykles...');
+        }
+        console.log({newErrors});
+
+        setErrors(newErrors);
+
+        if (!errors.length) {
+            
+            setUsers((prev) => [...prev, { name, email, password }]);
+        }
+    }
+
+
+
     return (
         <div className={style.registerPage}>
           <h2>Create your account</h2>
+          <div className={`${style.error} ${errors.length ? style.show : ''}`}>
+            {errors.map(err => <p>{err}</p>)}
+            </div>
           <form className={style.form}>
             <div className={style.row}>
                 <input onChange={updateName} value={name} id='name' type='text' placeholder='Name' />
@@ -26,19 +81,19 @@ export function Register() {
                 <input onChange={updateEmail} value={email} id='email' type='text' placeholder='Email' />
             </div>
             <div className={style.row}>
-                <input id='password' type='password' placeholder='Password' />
+                <input onChange={updatePassword} value={password} id='password' type='password' placeholder='Password' />
             </div>
             <div className={style.checkboxStyle}>
-                <input type="checkbox" id="checkbox"></input>
-                    <label for="checkbox">Agree to our TOS <link rel="stylesheet" href="" />(read)</label>
+                <input onChange={updateCheck} type="checkbox" id="checkbox"></input>
+                    <label htmlFor="checkbox">Agree to our TOS <link rel="stylesheet" />(read)</label>
                 
             </div>
             
           </form>
-          
-          <Button buttonTo="/feature1 "title="Register"/>
+          {/* buttonTo="/feature1" */}
+          <button onClick={registerUser} type="submit" className={button.button} >Register</button>
           <p>or</p>
-          <Button buttonTo="/feature1 "title="Login"/>
+          <Button buttonTo="/login" title="Login"/>
         </div>
     );
   }
