3332b9f05f40ce003e27bee222707b70e92b6705
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-28 14:17:24 +0300

    content page creation

---
+++ src/pages/Content.jsx
@@ -0,0 +1,15 @@
+import style from './Content.module.css';
+
+export function Content() {
+
+  
+    return (
+        <div className={style.contentPage}>
+          <h2>Cia as Simas</h2>
+          <img  className={style.img} src="./img/mentor5.JPG" alt="Simas" />
+          
+        </div>
+    );
+  }
+
+
