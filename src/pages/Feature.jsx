cfa02dd086fc11182712e8596576a81dc11b6741
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-07-28 14:16:53 +0300

    feature page update

--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -23,7 +23,8 @@ export function Feature({data, dataFeatureLength}) {
 
     return (
         <div className={style.featurePage}>
-          <div >{data.icon}</div>
+          {/* <div >{data.icon}</div> */}
+          <img className={style.img} src={`../img/${data.icon}`} alt="" />
           <h2>{data.title}</h2>
           <p>{data.text}</p>
           <div className={style.bubble}>
