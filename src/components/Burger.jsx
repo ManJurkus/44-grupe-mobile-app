26466df61ca494d15037fea94d55261bd36b5365
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-08-01 15:45:50 +0300

    burger update

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,5 +1,6 @@
 import style from './Burger.module.css';
 import { TbMenuOrder } from 'react-icons/tb';
+import { GrFormClose } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -17,6 +18,7 @@ export function Burger() {
         <div className={style.burger}>
           <TbMenuOrder onClick={updateBurger} size="50px" />
           <div className={`${style.burgerMeniu} ${burgerSwich ? style.show : ''}`}>
+            <GrFormClose onClick={updateBurger} size="30px" />
             <Link className={style.burgerLink} to='/Something'>Something</Link>
             <Link  className={style.burgerLink} to='/Something'>Something</Link>
             <Link className={style.burgerLink} to='/Something'>Something</Link>
